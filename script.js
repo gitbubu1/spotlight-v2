e0e20e4ac0b8a1ba73754c0f14b0de8b2b0bf0a5
Bubu Munda <bubumunda555@gmail.com>
2025-11-27 16:40:04 +0530

    mute btn added

--- script.js
+++ script.js
@@ -1,7 +1,18 @@
 
 addEventListener("mousemove", (e) => {
-    console.log(e.clientX, e.clientY);
+  document.body.style.setProperty("--x", e.clientX + "px");
+  document.body.style.setProperty("--y", e.clientY + "px");
+});
 
-    document.body.style.setProperty("--x", e.clientX + "px");
-    document.body.style.setProperty("--y", e.clientY + "px");
+
+const video = document.getElementById("bg-video");
+const btn = document.getElementById("soundToggle");
+
+btn.addEventListener("click", () => {
+  
+
+  video.muted = !video.muted;
+  
+ 
+  btn.textContent = video.muted ? "🔊" : "🔇";
 });
